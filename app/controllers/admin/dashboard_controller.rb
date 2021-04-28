135838e50cac9e40a4d597c8588da28b2c2eb214
Cole Kubossek <cole.kubossek@gmail.com>
2021-04-28 22:38:18 +0000

    updates admin dashboard with information

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
 
   def show
+    @totalp ||= Product.all.count()
+
+    @totalc ||= Category.all.count()
+
   end
 end
