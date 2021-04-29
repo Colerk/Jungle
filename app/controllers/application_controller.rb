06357c4aaad8789772d3204f558af3026ff82fb9
Cole Kubossek <cole.kubossek@gmail.com>
2021-04-29 19:51:55 +0000

    finishes user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
 
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+  
   private
 
   def cart
@@ -12,7 +21,7 @@ class ApplicationController < ActionController::Base
 
   def enhanced_cart
     @enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
-  end
+end
   helper_method :enhanced_cart
 
   def cart_subtotal_cents
