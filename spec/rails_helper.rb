d58c21aa3b9fcfbed2b9f0c97c0553d7d54c0f48
Cole Kubossek <cole.kubossek@gmail.com>
2021-05-04 20:09:39 +0000

    sets up capybara testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,28 +1,22 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'devise'
 
-require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 
-module AuthHelper
-  def login_user
-   @request.env["devise.mapping"] = Devise.mappings[:user]
-   user = FactoryGirl.create(:user)
-   # user.confirm # or set a confirmed_at inside the factory. Only      necessary if you are using the "confirmable" module
-   sign_in user
-  end
- end
-
- RSpec.configure do |config|
-  config.include Devise::TestHelpers, :type => :controller
-end
 
 require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
 # Add additional requires below this line. Rails is not loaded until this point!
+require "capybara/rails"
+require "capybara/rspec"
+require "capybara/poltergeist"
+require 'spec_helper'
+
+Capybara.javascript_driver = :poltergeist
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -37,7 +31,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -54,7 +48,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
